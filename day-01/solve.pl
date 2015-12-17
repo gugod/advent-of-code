185614087a2030b4b7344bbc08e72a8fc3fea016
Kang-min Liu <gugod@gugod.org>
2015-12-17 18:22:53 +0100

    rewrite to use substr.

--- day-01/solve.pl
+++ day-01/solve.pl
@@ -1,16 +1,14 @@
 use v5.22;
+open my $fh, "<", "input";
 local $/ = undef;
-my @todo = split("", scalar(<>));
+my $input = <$fh>;
 my %step = ( '(' => 1, ')' => -1 );
 my $santa_at = 0;
 my $santa_first_visit_to_basement;
-for my $i (0..$#todo) {
-    $santa_at+= $step{$todo[$i]};
+for my $i (1..length($input)) {
+    my $c = substr($input,$i-1,1);
+    $santa_at+= $step{$c};
     $santa_first_visit_to_basement //= ($i+1) if $santa_at == -1;
 }
 say "part 1: $santa_at";
 say "part 2: $santa_first_visit_to_basement";
-
-
-
-
