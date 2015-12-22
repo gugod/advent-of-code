a10d3abf70e41b6abfd696c9019fd543da9d887b
Kang-min Liu <gugod@gugod.org>
2015-12-22 22:50:39 +0100

    day-08 part2 solution

--- day-08/solve.perl5.pl
+++ day-08/solve.perl5.pl
@@ -1,6 +1,6 @@
 use v5.20;
-sub count_it {
-    my ($line) = @_;
+sub iter_over_char {
+    my ($line, $cb) = @_;
     my ($mem,$code) = (0,0);
     $code = length($line);
     my $x = undef;
@@ -9,14 +9,14 @@ sub count_it {
         if (defined($x)) {
             # \\ \" \xNN
             if ($c eq "\\" || $c eq '"') {
-                $mem++;
+                $cb->($c, "\\".$c);
                 $x = undef;
             } elsif ($c eq "x") {
                 $x = "x";
             } elsif (substr($x,0,1) eq "x" && $c =~ /[0-9a-f]/) {
                 $x .= $c;
                 if (length($x) == 3) {
-                    $mem++;
+                    $cb->(chr(hex(substr($x,1,2))), "\\".$x);
                     $x = undef;
                 }
             } else {
@@ -25,12 +25,33 @@ sub count_it {
         } elsif ($c eq "\\") {
             $x = "";
         } else {
-            $mem++;
+            $cb->($c, $c);
             $x = undef;
         }
     }
 
     return ($mem,$code);
+}
+
+sub count_it {
+    my ($line) = @_;
+     my ($mem,$code) = (0,0);
+    $code = length($line);
+    iter_over_char($line, sub { $mem++ });
+    return ($mem, $code);
+}
+
+sub encode_it {
+    my ($line) = @_;
+    my $enc = '"\"';
+    iter_over_char($line, sub {
+                       my ($c, $orig) = @_;
+                       $orig =~ s!\\!\\\\!g;
+                       $orig =~ s!"!\\"!g;
+                       $enc .= $orig;
+                   });
+    $enc .= '\""';
+    return $enc;
 }
 
 my @input;
@@ -46,3 +67,10 @@ for (@input) {
     $total += $code - $mem;
 }
 say "Part 1: $total";
+
+$total = 0;
+for (@input) {
+    my $x = encode_it($_);
+    $total += length($x) - length($_);
+}
+say "Part 2: $total";
