88def12401dda6ac7251cb80c0ab4c1bc6140ca8
Kang-min Liu <gugod@gugod.org>
2015-12-25 17:02:33 +0100

    day-13 part 2 solution.

--- day-13/solve.perl5.pl
+++ day-13/solve.perl5.pl
@@ -6,14 +6,35 @@ sub compute {
     my ($cost, $lookup, $p) = @_;
 
     my $total_cost = 0;
+    my $total_cost2 = 0;
+
     for my $j (0..$#$p) {
         my $i = ($j == 0) ? $#$p : $j-1;
         my $k = ($j == $#$p) ? 0 : $j+1;
-        $total_cost += $cost->{$lookup->[$p->[$j]]}{$lookup->[$p->[$k]]} // die "Unknown cost: $lookup->[$j] => $lookup->[$k]";
-        $total_cost += $cost->{$lookup->[$p->[$j]]}{$lookup->[$p->[$i]]} // die "Unknown cost: $lookup->[$j] => $lookup->[$i]";
+        $total_cost += $cost->{$lookup->[$p->[$j]]}{$lookup->[$p->[$k]]} // 0;
+        $total_cost += $cost->{$lookup->[$p->[$j]]}{$lookup->[$p->[$i]]} // 0;
     }
     return $total_cost;
 }
+
+sub compute_maximum {
+    my ($happiness, $guests) = @_;
+    my @guests = @$guests;
+    my $first_permutation = "0" . join("", 1 .. @guests-1);
+    my $last_permutation  = "0" . join("", reverse(1 .. @guests-1));
+    my %path_length;
+    my $max = -1;
+    for my $p ($first_permutation .. $last_permutation) {
+        my @p = split "", $p;
+        next unless uniq(grep { $_ < @guests } @p) == @guests;
+        my ($cost, $cost2) = compute($happiness, \@guests, \@p);
+        if ($max < $cost) {
+            $max = $cost;
+        }
+    }
+    return $max;
+}
+
 
 open my $fh, "<", "input";
 my @input = <$fh>;
@@ -28,20 +49,12 @@ for (@input) {
     }
 }
 
-my %total_happiness;
-my @guests = sort keys %happiness;
+my (@guests, $max);
 
-my $first_permutation = "0" . join("", 1 .. @guests-1);
-my $last_permutation = "0" . join("", reverse(1 .. @guests-1));
-my %path_length;
-my ($min,$max) = (999999999,-1);
-for my $p ($first_permutation .. $last_permutation) {
-    my @p = split "", $p;
-    next unless uniq(grep { $_ < @guests } @p) == @guests;
-    
-    my $cost = $total_happiness{$p} = compute(\%happiness, \@guests, \@p);
-    # say $total_happiness{$p} . "\t" . join(",", map { $guests[$_] } @p);
-    $max = $cost if $max < $cost;
-    $min = $cost if $min > $cost;
-}
-say "Min:$min Max:$max";
+@guests = keys %happiness;
+$max = compute_maximum(\%happiness, \@guests);
+say "Part 1: $max"; 
+
+@guests = ("Me", keys %happiness);
+$max = compute_maximum(\%happiness, \@guests);
+say "Part 2: $max";
