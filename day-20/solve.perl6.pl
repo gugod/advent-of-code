50ba8e6847f4e65095026c571c6760499aecff03
Kang-min Liu <gugod@gugod.org>
2016-01-03 23:44:29 +0100

    day-20 part1: alternative implementation.
    Remove the need to call .is-prime

--- day-20/solve.perl6.pl
+++ day-20/solve.perl6.pl
@@ -1,26 +1,21 @@
 use v6;
 
-sub prime-factorization($x) {
-    my %pf;
-    unless ( $x.is-prime ) {
-        my $x2 = $x;
-        for (2 .. $x/2).grep({ .is-prime && $x %% $_ }) -> $f {
-            %pf{$f} = (1..log($x2,$f)).reverse.first({ $x2 %% ($f**$_) });
-            $x2 /= ($f ** %pf{$f});
+multi sub sum-of-factors($n is copy) {
+    my $sum = 1;
+    for 2 .. sqrt($n) -> $f {
+        my $p = 1;
+        while ($n %% $f) {
+            $p = $p*$f + 1;
+            $n /= $f;
         }
+        $sum *= $p;
     }
-    return %pf;
+    $sum *= 1+$n if $n > 1;
+    return $sum;
 }
 
 sub presents($house) {
-    my $s;
-    if ($house.is-prime) {
-        $s = 1+$house;
-    } else {
-        my %pf = prime-factorization($house);
-        $s = [*] %pf.pairs.map({ my ($f,$p) = (.key, .value); [+] (0..$p).map({ $f**$_ }) });
-    }
-    return $s * 10; 
+    return sum-of-factors($house) * 10;
 }
 
 sub solve-part-one($input) {
@@ -28,8 +23,8 @@ sub solve-part-one($input) {
     for 1..* {
         my $p = presents($_);
         if ($p > $m) {
-            $m = $p;
             say "$_ => $p";
+            $m = $p;
         }
         if $p >= $input {
             say "Part 1: $_";
