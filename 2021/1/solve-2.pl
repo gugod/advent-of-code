b5c893f9c0a7700d67aba72772de0fe683abafb6
Kang-min Liu <gugod@gugod.org>
2021-12-01 19:24:53 +0900

    rename

--- 2021/1/solve-2.pl
+++ 2021/1/solve-2.pl
@@ -1,14 +1,13 @@
 #!/usr/bin/env perl
 use v5.34;
-my @nums = do {
+
+my @lines = do {
     open my $fh, "<", "input-1";
     <$fh>;
 };
 
-my @wins = map {
-    $nums[$_] + $nums[$_ - 1] + $nums[$_ - 2]
-} 2..$#nums;
+my @nums = map { $lines[$_] + $lines[$_ - 1] + $lines[$_ - 2] } 2..$#lines;
 
-my $incs = grep { $wins[$_] > $wins[$_-1] } 1...$#wins;
+my $incs = grep { $nums[$_] > $nums[$_-1] } 1...$#nums;
 
 say $incs;
