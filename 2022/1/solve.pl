b5785bc40826a7c1d19b846afa610c0f5188e79b
Kang-min Liu <gugod@gugod.org>
2022-12-02 13:43:55 +0900

    optimize: doing TopK with quickSelect

--- 2022/1/solve.pl
+++ 2022/1/solve.pl
@@ -13,10 +13,42 @@ sub solvePart1 ($caloriesPerElf) {
     say $maxCalorie;
 }
 
+sub quickSelectTopK( $nums, $k ) {
+    my $start = 0;
+    my $end = $#$nums;
+
+    while ($end > $start) {
+        my $pivotPos = $start + int rand($end - $start + 1);
+        my $pivot = $nums->[$pivotPos];
+
+        ($nums->[ $pivotPos ], $nums->[ $end ]) = ($nums->[ $end ], $nums->[ $pivotPos ]);
+        $pivotPos = $end;
+
+        my $i = $start;
+        for my $j ( $start .. $end - 1 ) {
+            if ($nums->[$j] > $pivot) {
+                ($nums->[$i], $nums->[$j]) = ($nums->[$j], $nums->[$i]);
+                $i++;
+            }
+        }
+
+        ($nums->[$i], $nums->[$pivotPos]) = ($nums->[$pivotPos], $nums->[$i]);
+        $pivotPos = $i;
+
+        if ($pivotPos < $k) {
+            $start = $pivotPos;
+        } else {
+            $end = $pivotPos - 1;
+        }
+    }
+
+    return @$nums[0..$k-1];
+}
+
 sub solvePart2 ($caloriesPerElf) {
-    my @sorted = sort { $b <=> $a } map { sum(@$_) } @$caloriesPerElf;
-    my @topThree = @sorted[0, 1, 2];
-    say sum(@topThree);
+    my @calories = map { sum(@$_) } @$caloriesPerElf;
+    my @top3 = quickSelectTopK( \@calories, 3 );
+    say sum(@top3);
 }
 
 die "I need input" unless $ARGV[0];
