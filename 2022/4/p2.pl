48da6af6e17c50a32987283f119e403c8ba904e1
Kang-min Liu <gugod@gugod.org>
2022-12-04 22:05:10 +0900

    even more simplified

--- 2022/4/p2.pl
+++ 2022/4/p2.pl
@@ -3,5 +3,6 @@ use List::Util qw(min max);
 open my $fh, "<", "input.txt";
 say 0+ grep {
     my ($a,$b,$c,$d) = $_ =~ m/([0-9]+)/g;
-    max($a, $c) <= min($b, $d)
+    # !($b < $c || $d < $a)
+    $c <= $b && $a <= $d
 } <$fh>;
--- 2022/4/p2.raku
+++ 2022/4/p2.raku
@@ -2,5 +2,5 @@
 say elems "input.txt".IO.lines.grep: {
     my ($a,$b,$c,$d) = $_.comb(/<[0..9]>+/);
 
-    ($c ≤ ($a|$b) ≤ $d) || ($a ≤ ($c|$d) ≤ $b)
+    ($c ≤ ($a|$b) ≤ $d)
 };
