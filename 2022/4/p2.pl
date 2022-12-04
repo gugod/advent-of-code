f32154c09b5db0e827e7d1f0ae68ff99c361d0ff
Kang-min Liu <gugod@gugod.org>
2022-12-04 14:16:12 +0900

    Ocassionally the scene with chained operator in action.

---
+++ 2022/4/p2.pl
@@ -0,0 +1,14 @@
+use v5.36;
+open my $fh, "<", "input.txt";
+
+my $count = 0;
+while (<$fh>) {
+    my @bounds = $_ =~ m/([0-9]+)/g;
+    $count++ if (
+           ($bounds[2] <= $bounds[0] <= $bounds[3])
+           || ($bounds[2] <= $bounds[1] <= $bounds[3])
+           || ($bounds[0] <= $bounds[2] <= $bounds[3] <= $bounds[1])
+           || ($bounds[2] <= $bounds[0] <= $bounds[1] <= $bounds[3])
+    );
+}
+say $count;
