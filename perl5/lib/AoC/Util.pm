b7c4b13ebbc566d9e684e565a5fde0c04e62ca33
Kang-min Liu <gugod@gugod.org>
2022-12-07 00:43:06 +0900

    a rotor sub for perl.

--- perl5/lib/AoC/Util.pm
+++ perl5/lib/AoC/Util.pm
@@ -2,7 +2,7 @@ use v5.36;
 package AoC::Util {
     use Exporter 'import';
 
-    our @EXPORT_OK = qw(count);
+    our @EXPORT_OK = qw(count rotor);
 
     sub count :prototype(&@) {
         my $predicate = shift;
@@ -13,5 +13,14 @@ package AoC::Util {
         return $count;
     }
 
+    sub rotor ( $size = 1, $skip = 0, @xs ) {
+        my @ys;
+        my $end = $size - 1;
+        while ($end < @xs) {
+            push @ys, [ @xs[ ($end - $size + 1) .. $end ] ];
+            $end += $size + $skip;
+        }
+        return @ys;
+    }
 };
 1;
