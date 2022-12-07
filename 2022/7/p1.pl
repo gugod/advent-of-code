deab1f1094abda91c991945c7b77ae98b3061529
Kang-min Liu <gugod@gugod.org>
2022-12-07 15:07:12 +0900

    this is exactly why I only buys SSDs larger than 512GB

---
+++ 2022/7/p1.pl
@@ -0,0 +1,48 @@
+use v5.36;
+use File::Slurp qw(slurp);
+use List::Util qw(sum);
+
+my $in = slurp("input");
+
+my @dirs;
+my %subdirOf;
+my %sizeOf;
+my $cwd = "/";
+for my $line (split "\n", $in) {
+    if ($line =~ /^\$ cd (?<dir>.+)$/) {
+        if ($+{dir} eq "/") {
+            $cwd = "/";
+        } elsif ($+{dir} eq "..") {
+            $cwd =~ s{/[^/]+$}{};
+        } else {
+            $cwd .= "/" . $+{dir};
+        }
+    }
+    elsif ($line =~ /^\$ ls$/) {
+    }
+    elsif ($line =~ /^dir (?<subdir>.+)$/) {
+        my $dir = $cwd . "/" . $+{subdir};
+        push @dirs, $dir;
+        push @{$subdirOf{$cwd} //= []}, $dir;
+    }
+    elsif ($line =~ /^(?<size>[0123456789]+) (?<file>.+)$/) {
+        $sizeOf{$cwd} //= 0;
+        $sizeOf{$cwd} += $+{size};
+    }
+    else {
+        die "Strange: [$line]";
+    }
+}
+
+sub totalSizeOf ($dir) {
+    state %cache;
+    return $cache{$dir} //= do {
+        my $size = $sizeOf{$dir} // 0;
+        for my $subdir (@{ $subdirOf{$dir} // []}) {
+            $size += totalSizeOf($subdir);
+        }
+        $size;
+    };
+}
+
+say sum(grep { $_ <= 100000 } map { totalSizeOf($_) } @dirs);
