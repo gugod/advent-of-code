81eba4fec79f389b7627e9b1c618d29d9e1f5930
Kang-min Liu <gugod@gugod.org>
2022-12-09 19:13:09 +0900

    rewrite -- after realizing that my impl just happened to produce the correct result by chance

--- 2022/9/p1.pl
+++ 2022/9/p1.pl
@@ -11,10 +11,29 @@ my %V = (
     "L" => [0,-1], "R" => [0,1],
 );
 
-sub tailFollow ($dir) {
-    return if abs($T[0] - $H[0]) <= 1 && abs($T[1] - $H[1]) <= 1;
-    $T[0] = $H[0] - $V{$dir}[0];
-    $T[1] = $H[1] - $V{$dir}[1];
+sub tailFollow () {
+    my $dr = $H[0] - $T[0];
+    my $dc = $H[1] - $T[1];
+
+    return 0 if (abs($dr) <= 1 && abs($dc) <= 1);
+
+    my @moves;
+    if ($dr == 0 || $dc == 0) {
+        push @moves, "L" if $dc < -1;
+        push @moves, "R" if $dc > 1;
+        push @moves, "U" if $dr < -1;
+        push @moves, "D" if $dr > 1;
+    } else {
+        push @moves, $dc < 0 ? "L" : "R";
+        push @moves, $dr < 0 ? "U" : "D";
+    }
+
+    for my $dir (@moves) {
+        $T[0] += $V{$dir}[0];
+        $T[1] += $V{$dir}[1];
+    }
+
+    return 1;
 }
 
 for (@lines) {
@@ -22,7 +41,7 @@ for (@lines) {
     for (1..$steps) {
         $H[0] += $V{$dir}[0];
         $H[1] += $V{$dir}[1];
-        tailFollow($dir);
+        tailFollow();
         $U{"@T"}++;
     }
 }
