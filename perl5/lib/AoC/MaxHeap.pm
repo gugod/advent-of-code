724a2f50eea55469d7caa33450d7a209d4a10da1
Kang-min Liu <gugod@gugod.org>
2022-12-16 22:39:27 +0900

    make AoC::MaxHeap an opaque heap.

--- perl5/lib/AoC/MaxHeap.pm
+++ perl5/lib/AoC/MaxHeap.pm
@@ -3,9 +3,11 @@ use v5.36;
 package AoC::MaxHeap {
     use Scalar::Util qw( looks_like_number );
 
-    sub new ($class) {
+    sub new ($class, @args) {
         my $self = bless {
             __stash => [],
+            compare => sub ($p, $q) { ... },
+            @args
         }, $class;
 
         return $self;
@@ -19,6 +21,42 @@ package AoC::MaxHeap {
         return undef if @$stash == 0;
         return $stash->[0];
     }
+
+    sub pop($self) {
+        my $stash = $self->__stash;
+        return undef if @$stash == 0;
+        my $min = $stash->[0];
+
+        $stash->[0] = $stash->[ $#$stash ];
+        delete $stash->[ $#$stash ];
+
+        my $cmp = $self->{"compare"};
+        my $i = 0;
+        while (2*$i+1 <= $#$stash) {
+            my ($j1, $j2) = (2*$i+1, 2*$i+2);
+            my $j = ($j2 <= $#$stash && ( $cmp->($stash->[$j2], $stash->[$j1]) > 0 )) ? $j2 : $j1;
+            last if $cmp->($stash->[$i], $stash->[$j]) > 0;
+            ($stash->[$j], $stash->[$i]) = ($stash->[$i], $stash->[$j]);
+            $i = $j;
+        }
+
+        return $min;
+    }
+
+    sub push($self, @vals) {
+        my $stash = $self->__stash;
+        for my $v (@vals) {
+            CORE::push @$stash, $v;
+            my $j = $#$stash;
+            while ($j > 0) {
+                my $i = int(($j - 1) / 2);
+                last if $self->{"compare"}->($stash->[$i], $stash->[$j]) > 0;
+                ($stash->[$j], $stash->[$i]) = ($stash->[$i], $stash->[$j]);
+                $j = $i;
+            }
+        }
+    }
+
 };
 
 1;
--- perl5/t/MaxHeap.t
+++ perl5/t/MaxHeap.t
@@ -1,5 +1,7 @@
 use v5.36;
 use Test2::V0;
+use JSON::PP qw(encode_json);
+use AoC::MaxHeap;
 use AoC::MaxHeap::Str;
 use AoC::MaxHeap::Num;
 
@@ -11,6 +13,15 @@ sub maxheap_sort ($class, $elems) {
     my @out;
     push(@out, $heap->pop()) while $heap->size() > 0;
     return @out;
+}
+
+sub verify_maxheap_can_sort ($heap, $elems, $elems_sorted) {
+    $heap->push(@$elems);
+    is $heap->size, scalar(@$elems);
+
+    my @out;
+    push(@out, $heap->pop()) while $heap->size() > 0;
+    is \@out, $elems_sorted;
 }
 
 sub verify_maxheap_str_can_sort ($elems) {
@@ -42,6 +53,21 @@ subtest 'AoC::MaxHeap::Str', sub {
         my @elems = map { chr(97 + rand 26) } (1 .. (10+rand(100)));
         subtest "fuzz round $round -- input " . join(" ", @elems), sub {
             verify_maxheap_str_can_sort \@elems;
+        };
+    }
+};
+
+subtest 'AoC::MaxHeap', sub {
+    for my $round (1..100) {
+        my @elems = map { [ chr(97 + rand 26) ] } (1 .. (10+rand(100)));
+        my @elems_sorted = sort { $b->[0] cmp $a->[0] } @elems;
+        subtest "fuzz round $round -- input " . encode_json(\@elems), sub {
+            my $heap = AoC::MaxHeap->new(
+                "compare" => sub ($a, $b) {
+                    $a->[0] cmp $b->[0]
+                }
+            );
+            verify_maxheap_can_sort $heap, \@elems, \@elems_sorted;
         };
     }
 };
