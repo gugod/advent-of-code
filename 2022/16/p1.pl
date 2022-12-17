130590f269bf0ef10b8ae475ea66190978d973f5
Kang-min Liu <gugod@gugod.org>
2022-12-17 11:50:16 +0900

    reduce the delay by utilizing all part shortest path.
    skip all 0-nodes and let the weight between non-zero nodes be their
    shortest distance.

--- 2022/16/p1.pl
+++ 2022/16/p1.pl
@@ -11,34 +11,20 @@ sub main ( $input = "input" ) {
 
     my @openableValves =  grep { $G->{"rate"}{$_} > 0 } @{$G->{"nodes"}};
     my $opened = { map { $_ => 0 } @openableValves };
-    my @Q = (["AA", 30, 0, $opened, 0+@openableValves]);
+
+    my @Q;
+    push @Q, ["AA", 30, 0, $opened, 0+@openableValves];
 
     my %visited;
     while (@Q) {
         my $now = pop @Q;
         my ($valve, $minutesLeft, $eventualReleases, $opened, $openableValves) = @$now;
 
-        my $k = "$valve $minutesLeft $eventualReleases $openableValves";
-        next if $visited{$k};
-        $visited{$k} = true;
+        $max = $eventualReleases if $eventualReleases > $max;
 
-        if ($eventualReleases > $max) {
-            $max = $eventualReleases;
-            say gist $max, $minutesLeft, [ map { @$_} rev_nsort_by { $_->[1] } grep { $_->[1] > 0 } pairs %$opened];
-        }
+        next if $minutesLeft <= 0 || $openableValves == 0;
 
-        if ($minutesLeft <= 0) {
-            next;
-        }
-
-        if ($openableValves == 0) {
-            push @Q, [$valve, 0, $eventualReleases, $opened, $openableValves];
-            next;
-        }
-
-        my $rate = $G->{"rate"}{$valve};
-
-        if ($rate > 0 && $opened->{$valve} == 0) {
+        if ( (my $rate = $G->{"rate"}{$valve}) > 0 && $opened->{$valve} == 0 ) {
             push @Q, [
                 $valve,
                 $minutesLeft - 1,
@@ -46,22 +32,19 @@ sub main ( $input = "input" ) {
                 { %$opened, $valve => $minutesLeft },
                 $openableValves - 1
             ];
+            next;
         }
-        my @next = nextValvesToVisit( $G, $valve, $minutesLeft, $opened );
-        # if (@next) {
-        #     say gist $valve, [map { $_ => $G->{"rate"}{$_} } @next], $opened;
-        # }
-        for my $v (@next) {
+
+        for my $v (nextValvesToVisit( $G, $valve, $minutesLeft, $opened )) {
             my $d = shortestDistance( $G, $valve, $v );
-            if ($minutesLeft >= $d) {
-                push @Q, [
-                    $v,
-                    $minutesLeft - $d,
-                    $eventualReleases,
-                    $opened,
-                    $openableValves
-                ];
-            }
+            next unless $minutesLeft > $d;
+            push @Q, [
+                $v,
+                $minutesLeft - $d,
+                $eventualReleases,
+                $opened,
+                $openableValves
+            ];
         }
     }
 
@@ -134,16 +117,11 @@ sub shortestDistance ( $G, $v1, $v2 ) {
 sub nextValvesToVisit ( $G, $currentValve, $minutesLeft, $opened ) {
     return map {
         $_->[0]
-    } rev_nsort_by {
-        $_->[2]
     } grep {
-        $_->[2] > 0
-    } map {[
-        @$_,
-        $G->{"rate"}{$_->[0]} * ($minutesLeft - shortestDistance($G, $currentValve, $_->[0]) - 1)
-    ]} grep {
-        ($_->[0] ne $currentValve)
-            && ($G->{"rate"}{$_->[0]} > 0)
-            && ($opened->{ $_->[0] } == 0)
+        my $v = $_->[0];
+        ($v ne $currentValve)
+            && ($G->{"rate"}{$v} > 0)
+            && ($opened->{$v} == 0)
+            && shortestDistance($G, $currentValve, $v) < $minutesLeft
     } pairs %{ $G->{"allPairShortestPath"}{$currentValve} };
 }
