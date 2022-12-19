c509636798ea3c5f2b62a25ef5e451d85a87bf7b
Kang-min Liu <gugod@gugod.org>
2022-12-19 10:18:22 +0900

    re-implemented with Math::Vector::Real
    Much nicer.

--- 2022/18/p1.pl
+++ 2022/18/p1.pl
@@ -2,26 +2,20 @@ use v5.36;
 use FindBin '$Bin';
 use lib $Bin . '/../../perl5/lib';
 use AoC;
+use Math::Vector::Real;
 
 sub MAIN ( $input = "input" ) {
-    my @cubies = map { ArrayRef comb(qr/[0-9]+/, $_) } slurp($input);
+    my @cubies = map { V comb(qr/[0-9]+/, $_) } slurp($input);
 
-    my %cubeAt;
-    for my $c (@cubies) {
-        $cubeAt{"@$c"} = 1;
-    }
+    my %cubeAt = map { ("@$_" => 1) } @cubies;
 
-    my @sides = ([0,0,1],[0,1,0],[1,0,0],[0,0,-1],[0,-1,0],[-1,0,0]);
+    my @sides = map { V(@$_) } ([0,0,1],[0,1,0],[1,0,0],[0,0,-1],[0,-1,0],[-1,0,0]);
 
     my $count = 0;
     for my $c (@cubies) {
-        $count += sum(map { $cubeAt{"@$_"} ? 0 : 1 } map { vecplus($c,$_) } @sides);
+        $count += sum(map { $cubeAt{"@$_"} ? 0 : 1 } map { $c + $_ } @sides);
     }
     say $count;
 }
 MAIN( @ARGV );
 exit();
-
-sub vecplus ($x, $y) {
-    [ map { $x->[$_] + $y->[$_] } (0 .. $#$x) ]
-}
